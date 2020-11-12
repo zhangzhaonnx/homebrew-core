8f37d605946143f09abac96676174634704f48d6
ZhaoZhang <zhaozhang@thoughtworks.com>
2020-11-12 22:16:09 +0800

    updrate to v1.0.1

--- hello.rb
+++ hello.rb
@@ -1,12 +1,12 @@
 class Util < Formula
   desc ""
   homepage ""
-  url "https://github.com/zhangzhaonnx/util/archive/v1.0.0.tar.gz"
-  sha256 "795c7e99182c5f59e0b17297363a10d55a729ff7e4e40f8e4a637eda81c47d12"
+  url "https://github.com/zhangzhaonnx/util/archive/v1.0.1.tar.gz"
+  sha256 "c49f915e548b5915a89ae737af5379f114a73db1893d3c18cb686afbd72e5e81"
   license ""
 
   def install
-  	bin.install "util"
+  	bin.install "hello"
   end
 
 end
