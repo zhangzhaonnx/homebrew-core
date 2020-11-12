8798998ae9e1767b3969385a01b019951c88cda3
ZhaoZhang <zhaozhang@thoughtworks.com>
2020-11-12 21:25:58 +0800

    add formula

---
+++ util.rb
@@ -0,0 +1,12 @@
+class Util < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/zhangzhaonnx/util/archive/v1.0.0.tar.gz"
+  sha256 "795c7e99182c5f59e0b17297363a10d55a729ff7e4e40f8e4a637eda81c47d12"
+  license ""
+
+  def install
+  	bin.install "util"
+  end
+
+end
